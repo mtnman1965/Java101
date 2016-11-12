aacb6acba6780f3d4594c8563359518feeb65f6f
Ed Pfromer <epfromer@outlook.com>
2016-11-12 15:33:57 -0700

    added tests

--- src/test/java/com/tomoab/generics/hashMapTest.java
+++ src/test/java/com/tomoab/generics/hashMapTest.java
@@ -1,6 +1,7 @@
 package com.tomoab.generics;
 
 import com.tomoab.Person;
+import com.tomoab.Product;
 import org.junit.Test;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
@@ -9,8 +10,10 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * hashMapTest
@@ -18,6 +21,32 @@ import static org.junit.Assert.assertEquals;
 public class hashMapTest {
     @Test
     public void hashMapTestMain() throws Exception {
+        Product honda = new Product("Honda", 34000, 1800);
+        Product acura = new Product("Acura", 49000, 2200);
+        Product toyota = new Product("Toyota", 34500, 1500);
+        Product fiat = new Product("Fiat", 36000, 1800);
+
+        // create HashMap, add a few items, and print out
+        HashMap<String, Product> stringProductMap = new HashMap<>();
+        stringProductMap.put("acura", acura);
+        stringProductMap.put("honda", honda);
+        stringProductMap.put("toyota", toyota);
+        stringProductMap.put("fiat", fiat);
+        System.out.println(stringProductMap);
+        Set<String> strings = stringProductMap.keySet();
+        strings.forEach(s -> System.out.println(s + " => " + stringProductMap.get(s)));
+
+        System.out.println("overwrite fiat with a honda and print entire map");
+        stringProductMap.put("fiat", honda);
+        strings = stringProductMap.keySet();
+        strings.forEach(s -> System.out.println(s + " => " + stringProductMap.get(s)));
+
+        assertEquals(stringProductMap.size(), 4);
+        assertFalse(stringProductMap.isEmpty());
+    }
+
+    @Test
+    public void hashMapSorts() throws Exception {
         Person don = new Person("Don Draper", LocalDate.of(1925, 1, 1));
         Person peggy = new Person("Peggy Olson", LocalDate.of(1940, 1, 1));
         Person bert = new Person("Bert Cooper", LocalDate.of(1900, 1, 1));
