d3e48963b8820b3637af3eabe176d3380421d53c
Bike Dude <bikedude@outlook.com>
2016-12-13 07:09:26 -0800

    added test, fixed other

--- src/test/java/com/tomoab/ProductTest.java
+++ src/test/java/com/tomoab/ProductTest.java
@@ -15,9 +15,16 @@ public class ProductTest {
         assertEquals(honda.getWeight(), 1800);
         assertEquals(honda.getCost(), 34000);
 
-        Product toyota = new Product("Toyota", 29000, 1700);
+        // New Toyota
+        Product toyota = new Product("Toyota", 29000, 1600);
         assertEquals(toyota.getName(), "Toyota");
-        assertEquals(toyota.getWeight(), 1700);
+        assertEquals(toyota.getWeight(), 1600);
         assertEquals(toyota.getCost(), 29000);
+
+        // New Chevy
+        Product toyota = new Product("Chevy", 22000, 1900);
+        assertEquals(toyota.getName(), "Chevy");
+        assertEquals(toyota.getWeight(), 1900);
+        assertEquals(toyota.getCost(), 22000);
     }
 }
