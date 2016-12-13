a6bfb14f62194a9e37184fa0cd6ab5bd5ff80362
Mountain Man <mtnman1965@outlook.com>
2016-12-13 08:56:44 -0800

    added test

--- src/test/java/com/tomoab/ProductTest.java
+++ src/test/java/com/tomoab/ProductTest.java
@@ -23,9 +23,10 @@ public class ProductTest {
 
         // New Chevy
         // this is a heavy vehicle, but the price is right
-        Product toyota = new Product("Chevy", 22000, 1900);
+        // price isn't that low!
+        Product toyota = new Product("Chevy", 25000, 1900);
         assertEquals(toyota.getName(), "Chevy");
         assertEquals(toyota.getWeight(), 1900);
-        assertEquals(toyota.getCost(), 22000);
+        assertEquals(toyota.getCost(), 25000);
     }
 }
