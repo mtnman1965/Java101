b6ac65b217d9d524aee1231f126ac05029d2619d
Bike Dude <bikedude@outlook.com>
2016-12-14 09:47:02 -0800

    added lexus

--- src/test/java/com/tomoab/ProductTest.java
+++ src/test/java/com/tomoab/ProductTest.java
@@ -22,18 +22,22 @@ public class ProductTest {
         assertEquals(toyota.getCost(), 29000);
 
         // New Chevy
-        // this is a heavy vehicle, but the price is right
-        // price isn't that low!
         Product chevy = new Product("Chevy", 25000, 1950);
         assertEquals(chevy.getName(), "Chevy");
         assertEquals(chevy.getWeight(), 1950);
         assertEquals(chevy.getCost(), 25000);
 
-        // new porche
+        // new Porche
         Product porsche = new Product("Porsche 911", 89400, 3150);
         assertEquals(porsche.getName(), "Porsche 911");
         assertEquals(porsche.getWeight(), 3150);
         assertEquals(porsche.getCost(), 89400);
+
+        // new Lexus
+        Product lexus = new Product("Lexus RX", 43020, 3500);
+        assertEquals(lexus.getName(), "Lexus RX");
+        assertEquals(lexus.getWeight(), 3500);
+        assertEquals(lexus.getCost(), 43020);
 
     }
 }
