d83c8036a2ee63df69e3a07bfef3ea896693a7ea
Mountain Man <mtnman1965@outlook.com>
2016-12-14 12:29:50 -0800

    updated honda test

--- src/test/java/com/tomoab/ProductTest.java
+++ src/test/java/com/tomoab/ProductTest.java
@@ -10,10 +10,10 @@ import static org.junit.Assert.*;
 public class ProductTest {
     @Test
     public void getName() throws Exception {
-        Product honda = new Product("Honda", 34000, 1800);
-        assertEquals(honda.getName(), "Honda");
-        assertEquals(honda.getWeight(), 1800);
-        assertEquals(honda.getCost(), 34000);
+        Product hondaCivic = new Product("Honda Civic", 18740, 2742);
+        assertEquals(hondaCivic.getName(), "Honda Civic");
+        assertEquals(hondaCivic.getWeight(), 2742);
+        assertEquals(hondaCivic.getCost(), 18740);
 
         // New Toyota
         Product toyota = new Product("Toyota", 29000, 1600);
